a33b9ad7099fc666b1b4a9d78c295672663fc0df
elpreben <preben.nygard@gmail.com>
2025-07-17 14:40:08 +0200

    Delete components/ui directory

--- components/ui/textarea.js
+++
@@ -1,3 +0,0 @@
-export default function Textarea(props) {
-  return <textarea {...props} className={`w-full border rounded-xl p-2 h-32 resize-none focus:outline-none focus:ring focus:border-blue-500 ${props.className || ""}`} />;
-}
