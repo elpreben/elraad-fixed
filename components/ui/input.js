a80de6de03e6f7a96cad268bc4e08ae62153a22e
elpreben <preben.nygard@gmail.com>
2025-07-18 08:26:15 +0200

    Delete components/ui directory

--- components/ui/input.js
+++
@@ -1,3 +0,0 @@
-export default function Input(props) {
-  return <input {...props} className={`w-full border rounded-xl p-2 focus:outline-none focus:ring focus:border-blue-500 ${props.className || ""}`} />;
-}
